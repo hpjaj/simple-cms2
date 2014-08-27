bddc1a59eae06409fb8d573ccde174ff12381e30
hpjaj <hmlevine@gmail.com>
2014-08-27 10:11:00 -0600

    Exercise to practice creating my own controllers and views

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,7 +1,7 @@
 class SubjectsController < ApplicationController
 
   layout false
-  
+
   def index
     @subjects = Subject.sorted
   end
@@ -15,11 +15,15 @@ class SubjectsController < ApplicationController
   end
 
   def create
+    # Instantiate a new object using form parameters
     @subject = Subject.new(subject_params)
+    # Save the object
     if @subject.save
+      # If save succeeds, redirect to the index action
       flash[:notice] = "Subject created successfully."
       redirect_to(:action => 'index')
     else
+      # If save fails, redisplay the form so user can fix problems
       render('new')
     end
   end
@@ -29,11 +33,15 @@ class SubjectsController < ApplicationController
   end
 
   def update
+    # Find an existing object using form parameters
     @subject = Subject.find(params[:id])
+    # Update the object
     if @subject.update_attributes(subject_params)
+      # If update succeeds, redirect to the index action
       flash[:notice] = "Subject updated successfully."
       redirect_to(:action => 'show', :id => @subject.id)
     else
+      # If update fails, redisplay the form so user can fix problems
       render('edit')
     end
   end
@@ -48,9 +56,14 @@ class SubjectsController < ApplicationController
     redirect_to(:action => 'index')
   end
 
+
   private
 
     def subject_params
+      # same as using "params[:subject]", except that it:
+      # - raises an error if :subject is not present
+      # - allows listed attributes to be mass-assigned
       params.require(:subject).permit(:name, :position, :visible)
     end
+
 end
